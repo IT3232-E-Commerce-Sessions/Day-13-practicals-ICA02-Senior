bfdb28fd3868a6e8e570275a8d3e7ef21dfa77e3
Udara Nalawansa <udarakavishka13@gmail.com>
2025-06-19 23:02:57 +0530

    feat: Add model classes for E-Commerce application

---
+++ icae02/src/main/java/com/system/icae02/model/Seller.java
@@ -0,0 +1,57 @@
+package com.system.icae02.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Seller extends User{
+    private String companyName;
+    private String contactInfo;
+    @OneToMany(mappedBy = "seller")
+    @JsonIgnore
+    private List<Product>products;
+    
+    public Seller() {
+    	
+    }
+    
+    
+
+	public Seller(String companyName, String contactInfo, List<Product> products) {
+		super();
+		this.companyName = companyName;
+		this.contactInfo = contactInfo;
+		this.products = products;
+	}
+
+
+
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getContactInfo() {
+		return contactInfo;
+	}
+
+	public void setContactInfo(String contactInfo) {
+		this.contactInfo = contactInfo;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+}
